b5dd8e53c02bbb7a03f80c4ac264582bd3c70e03
Androidy <29017362+SiLeNSwOrD@users.noreply.github.com>
2022-09-29 03:36:46 +0100

    feature: added build date

--- build.js
+++ build.js
@@ -1,10 +1,15 @@
 const exe = require('@angablue/exe');
+const date = new Date();
+
+const day = date.toDateString();
+const time = date.toLocaleTimeString();
 
 const build = exe({
     entry: './index.js',
     out: './build/ytdl-v2.exe',
 });
 
+
 build.then(() => {
-    console.log('Done!');
+    console.log('Done! was built on '  + day + ' at ' + time);
 });

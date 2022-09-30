c8d9e99ca0c5669b2044829f4cab67abe2a8559b
Androidy <29017362+SiLeNSwOrD@users.noreply.github.com>
2022-09-30 08:15:13 +0100

    update: added windows properities to build exe

--- build.js
+++ build.js
@@ -4,9 +4,19 @@ const date = new Date();
 const day = date.toDateString();
 const time = date.toLocaleTimeString();
 
+const config = require('./config/config.json');
+
 const build = exe({
     entry: './index.js',
     out: './build/ytdl-v2.exe',
+    version: config.version,
+    properties: {
+        fileDescription: 'YTDL V2',
+        fileVersion: config.version,
+        LegalCopyright: 'Copyright 2022 - Androidy',
+        originalFilename: 'YTDL V2',
+        productName: 'YTDL V2',
+    },
 });
 
 
